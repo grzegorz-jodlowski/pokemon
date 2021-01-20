879ba42f909c528fcca3f8e5dc05e7d079029b98
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2021-01-20 18:53:14 +0100

    Add and style Pokemons list item

--- src/components/features/Pokemon/Pokemon.js
+++ src/components/features/Pokemon/Pokemon.js
@@ -2,18 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 
-import styles from './PokemonList.module.scss';
+import styles from './Pokemon.module.scss';
 
-const Component = ({ className }) =>
-  <main className={clsx(className, styles.root)}>
-    PokemonList
-  </main>;
+import { capitalizeFirstLetter } from '../../../utils/capitalizeFirstLetter';
+
+import { ButtonLink } from '../../common/ButtonLink/ButtonLink';
+
+
+const Component = ({ className, pokemon }) =>
+  <article className={clsx(className, styles.root)}>
+    <p className={styles.title} >{capitalizeFirstLetter(pokemon.name)}</p>
+    <ButtonLink text='See more' href={`${process.env.PUBLIC_URL}/pokemons/${pokemon.name}`} className={styles.button} />
+  </article>;
 
 Component.propTypes = {
   className: PropTypes.string,
+  pokemon: PropTypes.object,
 };
 
 export {
-  Component as PokemonList,
+  Component as Pokemon,
 };
 
